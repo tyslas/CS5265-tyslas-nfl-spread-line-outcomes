5c8be0846f5c6e521b1ca3f659a5a6ff44375dd7
Tito Yslas <roberto.yslas@gmail.com>
2023-06-14 00:14:15 -0600

    add mapping for team IDs

---
+++ team_ids.py
@@ -0,0 +1,54 @@
+team_ids = {
+    'San Francisco 49ers': 'SF',
+    'Dallas Cowboys': 'DAL',
+    'Pittsburgh Steelers': 'PIT',
+    'Green Bay Packers': 'GB',
+    'Philadelphia Eagles': 'PHI',
+    'Minnesota Vikings': 'MIN',
+    'Denver Broncos': 'DEN',
+    'Miami Dolphins': 'MIA',
+    'Kansas City Chiefs': 'KC',
+    'Buffalo Bills': 'BUF',
+    'Chicago Bears': 'CHI',
+    'New York Giants': 'NYG',
+    'Atlanta Falcons': 'ATL',
+    'New Orleans Saints': 'NO',
+    'New York Jets': 'NYJ',
+    'Detroit Lions': 'DET',
+    'Cincinnati Bengals': 'CIN',
+    'New England Patriots': 'NE',
+    'Washington Redskins': 'WAS',
+    'Cleveland Browns': 'CLE',
+    # should be SD but all the data uses LAC
+    'San Diego Chargers': 'LAC',
+    'Seattle Seahawks': 'SEA',
+    'Tampa Bay Buccaneers': 'TB',
+    # should be OAK but all the data uses LVR
+    'Oakland Raiders': 'LVR',
+    'Indianapolis Colts': 'IND',
+    'Los Angeles Rams': 'LAR',
+    'Arizona Cardinals': 'ARI',
+    # should be HOU but all the data uses TEN
+    'Houston Oilers': 'TEN',
+    'Carolina Panthers': 'CAR',
+    'Jacksonville Jaguars': 'JAX',
+    'Baltimore Ravens': 'BAL',
+    'Tennessee Titans': 'TEN',
+    # should be STL but all the data uses LAR
+    'St. Louis Rams': 'LAR',
+    'Houston Texans': 'HOU',
+    # should be STL but all the data uses ARI
+    'St. Louis Cardinals': 'ARI',
+    'Baltimore Colts': 'BAL',
+    # should be LAR but all the data uses LVR
+    'Los Angeles Raiders': 'LVR',
+    'Los Angeles Chargers': 'LAC',
+    # should be PHX but all the data uses ARI
+    'Phoenix Cardinals': 'ARI',
+    # 'Boston Patriots': '', the franchise changed the name of their team to the New England Patriots in 1971
+    # the data does not have any team_favorite_id listed before the 12/24/78 in the 1978 season (row 2494)
+    'Las Vegas Raiders': 'LVR',
+    'Washington Football Team': 'WAS',
+    'Tennessee Oilers': 'TEN',
+    'Washington Commanders': 'WAS',
+}
